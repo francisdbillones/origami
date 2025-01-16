3bc1301a4a3567ff795279f3bacb8f9a5399a2e7
francisdbillones <francisd.billones@gmail.com>
2025-01-16 10:18:01 +0800

    uses integers now, works for any arbitrary long sequence

--- ByteStaticModel.cpp
+++ ByteStaticModel.cpp
@@ -2,43 +2,49 @@
 #include <iostream>
 #include <vector>
 
+typedef struct frac {
+  uint64_t num;
+  uint64_t dem;
+} frac;
+
 class ByteStaticModel {
 private:
-  double probabilities[0xFF];
-  double cumulative_probabilities[0xFF];
+  frac probabilities[256];
+  frac cumulative_probabilities[256];
 
 public:
-  ByteStaticModel(const std::vector<uint8_t> &bytes,
-                  double smoothing_constant) {
-    uint64_t freqs[0xFF] = {0};
+  uint64_t count;
+  ByteStaticModel(const std::vector<uint8_t> &bytes, frac smoothing_constant) {
+    uint64_t freqs[256] = {0};
     for (uint8_t byte : bytes) {
       freqs[byte]++;
     }
 
-    for (int i = 0; i < 0xFF; ++i) {
-      probabilities[i] = (freqs[i] + smoothing_constant) /
-                         (bytes.size() + 0xFF * smoothing_constant);
+    for (int i = 0; i < 256; ++i) {
+      probabilities[i] = {freqs[i], bytes.size()};
     }
     cumulative_probabilities[0] = probabilities[0];
-    for (int i = 1; i < 0xFF; ++i) {
-      cumulative_probabilities[i] =
-          cumulative_probabilities[i - 1] + probabilities[i];
+    for (int i = 1; i < 256; i++) {
+      cumulative_probabilities[i] = {cumulative_probabilities[i - 1].num +
+                                         probabilities[i].num,
+                                     bytes.size()};
     }
+
+    count = bytes.size();
   }
 
-  double get_probability(uint8_t byte) const {
+  frac get_probability(uint8_t byte) const {
     return cumulative_probabilities[byte];
   }
-  std::pair<uint8_t, std::pair<double, double>>
-  get_byte_and_range(double prob) const {
-    if (prob < probabilities[0])
-      return {0, {0.0, probabilities[0]}};
-
-    for (int i = 1; i < 0xFF; ++i) {
-      if (prob < cumulative_probabilities[i]) {
-        return {(uint8_t)i,
-                {cumulative_probabilities[i - 1], cumulative_probabilities[i]}};
-      }
+  std::pair<uint8_t, std::pair<frac, frac>>
+  get_byte_and_range(uint64_t prob) const {
+    if (prob < cumulative_probabilities[0].num) {
+      return {0, {{0, count}, cumulative_probabilities[0]}};
     }
+    for (int i = 1; i < 256; ++i) {
+      if (prob < cumulative_probabilities[i].num)
+        return {i,
+                {cumulative_probabilities[i - 1], cumulative_probabilities[i]}};
+    };
   }
 };
--- arithmetic_coding.cpp
+++ arithmetic_coding.cpp
@@ -7,59 +7,137 @@
 #include "ByteStaticModel.cpp"
 
 #define SMOOTHING_CONSTANT 0.1
+#define MAX_CODE 0xFFFFFFFFFFFFU
+#define HALF 0x800000000000U
+#define ONE_FOURTH 0x400000000000U
+#define THREE_FOURTHS 0xC00000000000U
 
-std::pair<double, double> arithmetic_encode(std::vector<uint8_t> bytes,
-                                            ByteStaticModel &model) {
+void put_bit_plus_pending(bool bit, int &pending_bits,
+                          std::vector<bool> &bits) {
+  bits.push_back(bit);
 
-  double low = 0.0;
-  double high = 1.0;
+  for (int i = 0; i < pending_bits; ++i)
+    bits.push_back(!bit);
+  pending_bits = 0;
+}
 
-  double byte_low, byte_high, range;
+std::vector<bool> arithmetic_encode(std::vector<uint8_t> bytes,
+                                    ByteStaticModel &model) {
+
+  uint64_t low = 0;
+  uint64_t high = MAX_CODE;
+
+  uint64_t range;
+  frac byte_low, byte_high;
+
+  std::vector<bool> output_bits;
+  int pending_bits = 0;
+
   for (uint8_t byte : bytes) {
     if (byte == 0)
-      byte_low = 0.0;
+      byte_low = {0, 1};
     else
       byte_low = model.get_probability(byte - 1);
     byte_high = model.get_probability(byte);
-    range = high - low;
-    high = low + range * byte_high;
-    low = low + range * byte_low;
-    std::cout << low << " " << high << std::endl;
+    range = high - low + 1;
+    high = low + (range * byte_high.num) / byte_high.dem - 1;
+    low = low + (range * byte_low.num) / byte_low.dem;
+
+    while (1) {
+      if (high < HALF) {
+        put_bit_plus_pending(0, pending_bits, output_bits);
+      } else if (low >= HALF) {
+        put_bit_plus_pending(1, pending_bits, output_bits);
+      } else if (low >= ONE_FOURTH && high < THREE_FOURTHS) {
+        pending_bits++;
+        low -= ONE_FOURTH;
+        high -= ONE_FOURTH;
+      } else
+        break;
+      high = (high << 1) | 1;
+      low <<= 1;
+      high &= MAX_CODE;
+      low &= MAX_CODE;
+    }
   }
-  return {low, high};
+  pending_bits++;
+  if (low < ONE_FOURTH)
+    put_bit_plus_pending(0, pending_bits, output_bits);
+  else
+    put_bit_plus_pending(1, pending_bits, output_bits);
+  return output_bits;
 }
 
-std::vector<uint8_t> arithmetic_decode(double value, unsigned int size,
+std::vector<uint8_t> arithmetic_decode(std::vector<bool> bits,
+                                       unsigned int size,
                                        ByteStaticModel &model) {
   std::vector<uint8_t> result;
 
-  for (unsigned int i = 0; i < size; i++) {
-    auto [byte, range] = model.get_byte_and_range(value);
-    result.push_back(byte);
-    value = (value - range.first) / (range.second - range.first);
-  }
+  uint64_t high = MAX_CODE;
+  uint64_t low = 0;
+  uint64_t value = 0;
 
+  for (int i = 0; i < 48; ++i) {
+    value = (value << 1) | bits[i];
+  }
+  int bits_read = 48;
+  for (int i = 0; i < size; ++i) {
+    uint64_t range = high - low + 1;
+    uint64_t scaled_value = ((value - low + 1) * model.count - 1) / range;
+    auto [byte, prob] = model.get_byte_and_range(scaled_value);
+    result.push_back(byte);
+    high = low + (range * prob.second.num) / prob.second.dem - 1;
+    low = low + (range * prob.first.num) / prob.first.dem;
+    while (1) {
+      if (high < HALF) {
+        // do nothing
+      } else if (low >= HALF) {
+        value -= HALF;
+        low -= HALF;
+        high -= HALF;
+      } else if (low >= ONE_FOURTH && high < THREE_FOURTHS) {
+        value -= ONE_FOURTH;
+        low -= ONE_FOURTH;
+        high -= ONE_FOURTH;
+      } else
+        break;
+
+      low <<= 1;
+      high = (high << 1) + 1;
+      value = (value << 1) | bits[bits_read++];
+    }
+  }
   return result;
 }
 
 int main() {
-  std::string message = "i am francis";
+
+  std::string message =
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend "
+      "ex arcu, vel ornare turpis cursus id. Integer luctus vulputate ligula, "
+      "et convallis ligula eleifend nec. Phasellus et pellentesque enim, in "
+      "luctus urna. Aliquam fringilla vitae tortor nec fermentum. Morbi "
+      "convallis dolor vel lectus dignissim luctus. Proin commodo, tellus ac "
+      "scelerisque mattis, tellus ante elementum ex, eget lacinia lectus "
+      "tortor id sem. In hac habitasse platea dictumst. Ut molestie sit amet "
+      "libero viverra feugiat. Aenean sodales, mauris nec fermentum mollis, "
+      "leo elit lacinia magna, et euismod urna diam sed nibh. Proin id erat a "
+      "mi posuere dignissim. Pellentesque a sodales dui, in vestibulum ante. "
+      "Proin viverra dignissim felis. Lorem ipsum dolor sit amet, consectetur "
+      "adipiscing elit. ";
 
   std::vector<uint8_t> bytes(message.begin(), message.end());
-  ByteStaticModel model(bytes, SMOOTHING_CONSTANT);
+  ByteStaticModel model(bytes, {1, 10});
 
-  auto [low, high] = arithmetic_encode(bytes, model);
+  auto bits = arithmetic_encode(bytes, model);
 
-  std::cout << low << " " << high << std::endl;
+  std::cout << std::endl;
 
-  for (auto byte : bytes) {
-    std::cout << (char)byte << " " << model.get_probability(byte) << std::endl;
-  }
-
-  auto decoded_bytes =
-      arithmetic_decode((high + low) / 2, message.length(), model);
+  auto decoded_bytes = arithmetic_decode(bits, message.length(), model);
 
   std::cout << std::string(decoded_bytes.begin(), decoded_bytes.end())
             << std::endl;
+  std::cout << "original: " << bytes.size() * 8 << " bits" << std::endl;
+  std::cout << "encoded: " << bits.size() << " bits" << std::endl;
   ;
 }
